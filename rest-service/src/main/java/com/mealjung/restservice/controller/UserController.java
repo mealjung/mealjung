1ebec772f94c76d61c38fa526f6a477b235f1744
minkee mac <mkc0615@gmail.com>
2024-05-04 22:28:01 +0900

    fix: controller parameter annotation added

--- rest-service/src/main/java/com/mealjung/restservice/controller/UserController.java
+++ rest-service/src/main/java/com/mealjung/restservice/controller/UserController.java
@@ -5,10 +5,7 @@ import com.mealjung.restservice.model.UserModel;
 import com.mealjung.restservice.service.UserApplicationProvider;
 import com.mealjung.restservice.service.UserApplicationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,7 +29,7 @@ public class UserController {
     }
 
     @GetMapping
-    public List<UserModel> findAllUsersByAgeRange(UserSearchCondition condition) {
+    public List<UserModel> findAllUsersByAgeRange(@ModelAttribute UserSearchCondition condition) {
         return userApplicationProvider.findAllByAgeRange(condition.startAgeRange, condition.endAgeRange);
     }
 }
